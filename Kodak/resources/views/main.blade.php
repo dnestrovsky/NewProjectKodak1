5f055203c2eb0aed996fc116a0e3e80b38646648
dnestrovsky <serj2356@gmail.com>
2021-04-08 00:11:30 +0300

    New Design(10.0)

--- Kodak/resources/views/main.blade.php
+++ Kodak/resources/views/main.blade.php
@@ -8,26 +8,6 @@
     <section id="home">
         <div class="inner-width">
             <div class="content">
-                <div class="countdown-col col">
-                    <div class="time middle">
-          <span>
-            <div id="d">12</div>
-            Days
-          </span>
-                        <span>
-            <div id="h">06</div>
-            Hours
-          </span>
-                        <span>
-            <div id="m">35</div>
-            Minutes
-          </span>
-                        <span>
-            <div id="s">54</div>
-            Seconds
-          </span>
-                    </div>
-                </div>
                 <h2>@lang('main.serv_main')</h2>
                 <h1> </h1>
                 <div class="sm">
@@ -82,37 +62,4 @@
             </div>
         </section>
     </section>
-
-    <script type="text/javascript">
-
-        var comingdate = new Date("apr 10, 2021 00:00:00");
-
-        var d = document.getElementById('d');
-        var h = document.getElementById('h');
-        var m = document.getElementById('m');
-        var s = document.getElementById('s');
-
-        var x = setInterval(function(){
-            var now = new Date();
-            var des = comingdate.getTime() - now.getTime();
-            var days = Math.floor(des/(1000 * 60 * 60 * 24));
-            var hours = Math.floor(des%(1000 * 60 * 60 * 24) / (1000 * 60 *60));
-            var mins = Math.floor(des%(1000 * 60 * 60) / (1000 * 60));
-            var secs = Math.floor(des%(1000 * 60) / 1000);
-
-            d.innerHTML = getTrueNumber(days);
-            h.innerHTML = getTrueNumber(hours);
-            m.innerHTML = getTrueNumber(mins);
-            s.innerHTML = getTrueNumber(secs);
-
-            if(des <= 0) clearInterval(x);
-
-        },1000);
-
-        function getTrueNumber(x) {
-            if (x<10) return '0'+x;
-            else return x;
-        }
-
-    </script>
 @endsection
